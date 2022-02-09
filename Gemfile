04ecf91b7c82dc474913c8d690e2f918273fddde
Lewis Buckley <lewis@lewisbuckley.co.uk>
2022-02-09 23:03:01 +0000

    User model with has_secure_password

--- Gemfile
+++ Gemfile
@@ -30,8 +30,7 @@ gem "redis", "~> 4.0"
 # Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
 # gem "kredis"
 
-# Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
-# gem "bcrypt", "~> 3.1.7"
+gem "bcrypt", "~> 3.1.7"
 
 gem "tzinfo-data", platforms: %i[mingw mswin x64_mingw jruby]
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_08_230319) do
+ActiveRecord::Schema.define(version: 2022_02_09_225958) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,13 @@ ActiveRecord::Schema.define(version: 2022_02_08_230319) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["destination_id"], name: "index_trips_on_destination_id"
     t.index ["origin_id"], name: "index_trips_on_origin_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "list_items", "lists"
